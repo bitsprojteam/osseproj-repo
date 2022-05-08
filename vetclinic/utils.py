0987a3114fbab6250b851c2c8227414517e4ea34
mnoohu.teammate <ahmdnooh@gmail.com>
2022-05-08 13:58:34 +0300

    Added a file utility function in utils.py file

--- vetclinic/utils.py
+++ vetclinic/utils.py
@@ -1,5 +1,7 @@
 import uuid
 import datetime
+import argparse
+import os
 
 def str_uuid():
     """
@@ -12,3 +14,42 @@ def converter(dt):
     format = '%b %d %Y %I:%M%p'  # You have to specify the format either statically or dynamically
     datetime_str = datetime.datetime.strptime(dt, format)
     return datetime_str
+
+
+def str2unix(input_str: str) -> str:
+    r"""
+    Converts the string from \r\n line endings to \n
+
+    Parameters
+    ----------
+    input_str
+        The string whose line endings will be converted
+
+    Returns
+    -------
+        The converted string
+    """
+    r_str = input_str.replace('\r\n', '\n')
+    return r_str
+
+
+def dos2unix(source_file: str, dest_file: str):
+    """
+    Converts a file that contains Dos like line endings into Unix like
+
+    Parameters
+    ----------
+    source_file
+        The path to the source file to be converted
+    dest_file
+        The path to the converted file for output
+    """
+    # NOTE: Could add file existence checking and file overwriting
+    # protection
+    with open(source_file, 'r') as reader:
+        dos_content = reader.read()
+
+    unix_content = str2unix(dos_content)
+
+    with open(dest_file, 'w') as writer:
+        writer.write(unix_content)
